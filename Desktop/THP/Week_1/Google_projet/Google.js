6fc98b7814976f7d04292628f50544faad77a2ac
Kiitaan <32227576+Kiitaan@users.noreply.github.com>
2017-10-05 19:10:47 +0200

    Add files via upload

---
+++ Desktop/THP/Week_1/Google_projet/Google.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementById("cache").style.display = "block";
+    document.getElementById("bouton").style.display = "none";
+}
+
+setTimeout(function()
+
+{
+  alert("Hey le site xxxvidsxxx est trop bien ! Viens dessus please");
+
+},
+
+10000);
